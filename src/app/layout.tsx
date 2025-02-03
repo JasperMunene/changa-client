d161f5d64c3bbb821b8d412cea868ff2c9811ee1
JasperMunene <jaspermunene2022@gmail.com>
2025-02-03 12:36:06 +0300

    Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Raleway, } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const afacad = Raleway({
@@ -26,7 +27,10 @@ export default function RootLayout({
     >
       <html lang="en">
         <body className={`${afacad.className}  antialiased`}>
-          <main>{children}</main>
+          <main>
+            {children}
+            <Analytics />
+            </main>
           <Toaster />
         </body>
       </html>
